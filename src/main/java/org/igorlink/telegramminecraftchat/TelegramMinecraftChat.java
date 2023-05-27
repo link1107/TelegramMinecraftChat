9fa3b4c7a07bdea709669a63b0a8970f6a334f6b
link1107 <igortokach@gmail.com>
2023-05-27 23:39:53 +0200

    Initial commit

---
+++ src/main/java/org/igorlink/telegramminecraftchat/TelegramMinecraftChat.java
@@ -0,0 +1,17 @@
+package org.igorlink.telegramminecraftchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TelegramMinecraftChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/org/igorlink/telegramminecraftchat/telegrambot/Bot.java
@@ -0,0 +1,2 @@
+package org.igorlink.telegramminecraftchat.telegrambot;public class MinecraftTelegramBot {
+}
